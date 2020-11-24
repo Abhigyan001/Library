a07c5ffc67acc8a6b1a45235688ea5b5d1a20919
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-11-24 18:22:13 +0530

    create book contructor and addition to library function

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+let myLibrary = [];
+
+function Book(
+  title,
+  author,
+  no_of_pages = 0,
+  have_read = false
+) {
+  this.title = title;
+  this.author = author;
+  this.no_of_pages = no_of_pages;
+  this.have_read = have_read;
+}
+
+function addBookToLibrary() {
+  let book1 = new Book('A beautiful mind', 'John Doe', 500, false);
+  myLibrary.push(book1);  
+}
+
