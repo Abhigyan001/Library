b9d3a3faa066f4f7ee0f6c7b16dec72819fff780
Clayton Siby <claytonsiby@gmail.com>
2020-11-24 17:43:25 +0200

    Update UI & addBookToLibrary logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,21 @@
 let container = document.querySelector('.container');
+let addBook = document.getElementById('add-book');
+let form = document.querySelector('.add-book-form');
+let bookTitle = document.getElementById('book-title').value;
+let bookAuthor = document.getElementById('book-author').value;
+let bookPages = document.getElementById('book-pages').value;
+let readChoice = document.getElementById('read-book');
+let submitForm = document.getElementById('submit-book');
+let message = document.querySelector('.message');
 let myLibrary = [];
+
+// hide form
+form.style.display = 'none';
+
+// display form
+addBook.addEventListener('click', function() {
+  form.style.display = 'block';
+});
 
 function Book(title, author, no_of_pages = 0, have_read = false) {
   this.title = title;
@@ -8,12 +24,37 @@ function Book(title, author, no_of_pages = 0, have_read = false) {
   this.have_read = have_read;
 }
 
-function addBookToLibrary() {
-  let book1 = new Book('A beautiful mind', 'John Doe', 500, false);
-  myLibrary.push(book1);  
+function evaluateFormData(obj) {
+  if(obj.title === '' || obj.author === '' || obj.no_of_pages === 0 || obj.have_read === undefined) {
+    console.log('Please make sure all fields are filled & try again!');
+  } else {
+    myLibrary.push(obj);
+    console.log('You have successfully added a new book to the Library!');
+  }
 }
+
+function addBookToLibrary() {
+  // determine if user already read the book
+  readChoice.addEventListener('change', (e) => {
+    if(e.target.value == 'yes') {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  let userBook = new Book(bookTitle, bookAuthor, bookPages, readChoice);
+
+  // evaluate form data and push to the myLibrary array
+  evaluateFormData(userBook);
+}
+
+submitForm.addEventListener('click', addBookToLibrary);
 
 function displayBook() {
   // create div el to contain the book object
   let bookContainer = document.createElement('div');
 }
+
+// console.log('Hey I am here!');
+
